49ba0a78a6b8825a4c138ea0d9351b135b0c3559
Jonathan Thomas Dean <JonathanTomDean@Gmail.com>
2021-03-01 10:47:56 -0500

    Fin

---
+++ src/app/modules/music/display-music/display-music.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+import { Music } from "../../../shared/models/music";
+
+@Component({
+  selector: "app-display-music",
+  templateUrl: "./display-music.component.html",
+  styleUrls: ["./display-music.component.css"],
+})
+export class DisplayMusicComponent implements OnInit {
+  private musicsRoute = "http://localhost:3000/music";
+  public musics: Music[];
+
+  constructor(private http: HttpClient) {}
+
+  getMusics() {
+    this.http.get<Music[]>(this.musicsRoute).subscribe((musics) => {
+      this.musics = musics;
+      console.log("Musics", this.musics); 
+    });
+  }
+
+  ngOnInit() {
+    this.getMusics();
+  }
+}
